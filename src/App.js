578d3c3e35aab7555e6034975f7d456fd6232372
TheRitikDubey <70100519+TheRitikDubey@users.noreply.github.com>
2022-09-18 00:38:27 +0530

    Netflify error resolved
    During deployement we faced  few issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import Banner from "./Components/Banner";
-import Movies from "./Components/Movie";
-import Favourite from "./Components/Favourite";
 function App() {
   return (
     <>
